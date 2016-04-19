628406529aa70803cb02ed4844a496d07a9a41ea
Chelsea Johnson <cheljoh@gmail.com>
2016-04-19 14:54:51 -0600

    Question Test
    Started writing test for question endpoint.

--- spec/requests/question_endpoints_spec.rb
+++ spec/requests/question_endpoints_spec.rb
@@ -2,5 +2,8 @@ require "rails_helper"
 
 RSpec.decribe "Question Endpoints Spec", type: :request do
   it "returns personality test questions" do
+    Question.create(question: "Are you really who you say you are", category: "extraversion", 1)
+    Question.create(question: "Are you coo", category: "agreeableness", 1)
+    get "api/v1/questions"
   end
 end
