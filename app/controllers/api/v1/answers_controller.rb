4888cf74ff10a87edec4fcd1cf3ccc53d4eb302a
Chelsea Johnson <cheljoh@gmail.com>
2016-04-20 21:28:39 -0600

    New Route
    Added new route for receiving data.

---
+++ app/controllers/api/v1/answers_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class AnswersController < ApiController
+      respond_to :json
+
+      def create
+        # do calculations
+        # respond_to or with with the results
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get "/questions", to: "questions#index"
+      post "/answers", to: "answers#create"
     end
   end
 
